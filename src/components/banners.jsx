7d3cd658c23a4c498a90a9f265708298a0692b43
geniein <cubya0104@gmail.com>
2021-04-15 16:18:10 +0900

    banner

---
+++ src/components/banners.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+const banners = require.context('../assets/images/banners', true);
+export default class Banners extends Component {
+    constructor(props) {
+        super(props)
+        const selectedCharacterEventWish = this.props.getFormattedCharacterEventWish('kebabCase')
+        this.state = {
+          selectedBanner: 'beginners-wish',
+          selectedCharacterEventWish,
+          banners: {
+            'beginners-wish': 'Novice Wishes',
+            [selectedCharacterEventWish]: 'Character Event Wish',
+            'epitome-invocation': 'Weapon Event Wish',
+            'wanderlust-invocation': 'Standard Wish'
+          },
+          wishes: {
+            'beginners-wish': 'beginnersWish',
+            [selectedCharacterEventWish]: this.props.getFormattedCharacterEventWish('camelCase', selectedCharacterEventWish),
+            'epitome-invocation': 'epitomeInvocation',
+            'wanderlust-invocation': 'wanderlustInvocation'
+          },
+          wasBeginnersWishDisabled: false,
+          isSettingsPageVisible: false
+        }
+    
+      }
+};
